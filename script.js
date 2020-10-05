1bf1f923cd9439fab1103b41e6a10d6d9c344975
unknown <salatajakub@protonmail.com>
2020-10-05 21:04:02 +0200

    Literówka

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var tablica = [0, 5, 1, 6, 2];
-d3.select("body")
+let text = d3.select("body")
     .selectAll("p")
     .data(tablica)
-    .text((d)=>"Wcześniej instniejący akapit " +d)
+    .text((d)=>"Wcześniej istniejący akapit " +d)
     .enter().append("p")
     .text((d)=>"Wygenerowany akapit " + d);
